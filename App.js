0f25433312fe8ffbadf14b87dfc94e006bbd4add
VenkataGanesh-Dev <venkataganesh294@gmail.com>
2025-08-23 15:34:52 +0530

    component

--- App.js
+++ App.js
@@ -1,60 +1,115 @@
-/*
- <div id="parent">
-    <div id="child">  // nested elements
-      <h1>iam h1 tag</h1>
-      <h2>iam h2 tag</h2>
-    </div>
-    div id="child2"> 
-      <h1>iam h1 tag</h1>
-      <h2>iam h2 tag</h2>
-    </div>
-  </div>
-*/
+import React, { createElement } from "react"; // THIS REACT CAME FROM THE NODE MODULES
+import ReactDOM from "react-dom/client"; // THIS REACTDOM CAME FROM THE NODE MODULES
 
-// This react element is an object that converts into the (HTML) that is understand by the browser
+// /*
+//  <div id="parent">
+//     <div id="child">  // nested elements
+//       <h1>Namaste react</h1>
+//       <h2>iam h2 tag</h2>
+//     </div>
+//     div id="child2">
+//       <h1>iam h1 tag</h1>
+//       <h2>iam h2 tag</h2>
+//     </div>
+//   </div>
+// */
 
-// This is the core way to write the code
+// // This react element is an object that converts into the (HTML) that is understand by the browser
 
-const parent = React.createElement("div",{id: "parent"},[
-    React.createElement("div", { id: "child" },[
-      React.createElement("h1",{},"iam h1 tag"),
-      React.createElement("h2",{},"iam h2 tag")
-    ]),
+// // This is the core way to write the code
 
-    React.createElement("div", { id: "child2" },[   // If the elements have multiple elements e have to use [] and pass 
-      React.createElement("h1",{},"iam h1 tag"),    // the elements as  lists
-      React.createElement("h2",{},"iam h2 tag"),
-    ]),
-  ]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1", key: "child1" }, [
+//     React.createElement("h1", { key: "h1-child1" }, "namaste reactjs"),
+//     React.createElement("h2", { key: "h2-child1" }, "iam h2 tag")
+//   ]),
 
-  console.log(parent); 
-    
-  const root = ReactDOM.createRoot(document.getElementById("root")); 
+//   React.createElement("div", { id: "child2", key: "child2" }, [
+//     React.createElement("h1", { key: "h1-child2" }, "iam h1 tag"),
+//     React.createElement("h2", { key: "h2-child2" }, "iam h2 tag"),
+//   ]),
+// ]);
+// {/* <div id='parent'>
+//   <div></div>
+// </div> */}
 
-  root.render(parent)
+//   console.log(parent);
 
-  // --NOTE-- 
-  // In React, "appended" refers to the concept of adding new elements 
-  // In React, "render" refers to the process of displaying a component's user interface (UI) on the screen. 
+//   const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//   root.render(parent)
+
+//   // --NOTE--
+//   // In React, "appended" refers to the concept of adding new elements
+//   // In React, "render" refers to the process of displaying a component's user interface (UI) on the screen.
+
+// /*
+
+// // hello world program using React
+//   const heading = React.createElement(
+//     "h1",
+//     { id : "heading"},
+//     "hello world from react!");
+
+//     console.log(heading); // it will return the object
+
+//   const root = ReactDOM.createRoot(document.getElementById("root"));
+//   // in this root where all the code will run
+
+//   root.render(heading);
+
+//   */
+
+// ---------------EPISODE-2 (BASICS OF REACT)----------------
+
+//  React.createElement => js-obj =>when we render this obj on dom => it converts into HTML
+
+// React.createElement => ReactElement(js-obj) => HTMLElement(render)
+
+// const heading = React.createElement(
+//   "h1",
+//   {id: "heading"},
+//   "This is h1 tag"
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+//  ---JSX---
+
+// JSX(by parcel having the babel pacakage it is coverted into REACT.creaateElement) => React.createElement => ReactElement(js-obj) => HTMLElement(render)
+
+// const jsxHeading = <h1 id="heading">React using JSX</h1>;
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(jsxHeading);
+
+// const jsxHeading = (
+//   <h1 className="heading" tabIndex="5">
+//     React using JSX
+//   </h1>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(jsxHeading);
 
 
-/*
-
-// hello world program using React
-  const heading = React.createElement(
-    "h1", 
-    { id : "heading"}, 
-    "hello world from react!");
-
-    console.log(heading); // it will return the object
-    
-
-  const root = ReactDOM.createRoot(document.getElementById("root")); 
-  // in this root where all the code will run
-
-  root.render(heading);
-
-  */
+// -----React component----
+// 1) class based component(old way)
+// 2) functional component(New way)
 
 
-  // hello suriya
+// functional component-
+
+const fn = (a,b) => {a*b};
+                                 // both are same 
+const fn1 = (a,b) => {
+  return a*b;
+}
+
+const Heading1 = ()=> {
+  return <h1>React Functional Component </h1>
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 React 🚀 
+
+# parcel 
+-builds- 
+dev build,
+local sever,
+HMR- hot module replacement
+- file watching algrothim - wriiten in c++
+- caching - faster builds
+- Image optimization
+- minification
+- bundling
+- compress
+
