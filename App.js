ff14ac2c602c3160d3b70b89aaa33594b8b485e7
VenkataGanesh-Dev <venkataganesh294@gmail.com>
2025-08-28 19:34:16 +0530

    c

--- App.js
+++ App.js
@@ -7,8 +7,9 @@ import ReactDOM, { createRoot } from "react-dom/client";
  *  - Nav Items
  * Body
  *  -Search
- *  -RestarantContainer
- *  -RestarantCard
+ *  -RestaurantContainer
+ *  -RestaurantCard
+ *    -img, name of res,rating,devlivery time
  * Footer
  *  -Copyright
  *  -Links
@@ -16,8 +17,62 @@ import ReactDOM, { createRoot } from "react-dom/client";
  *  -contact
  */
 
-const header = () => {};
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3nVmIb5Rnq8Y9iLlqa5d8T9DKa7jBXV_bSQ&s"
+          alt="food logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// const StyleCard = {
+//  backgroundColor: "skyblue"
+// };
+
+const Restaurantcard = () => {
+  return (
+    <div
+      className="res-card"
+      // style={StyleCard} THIS IS INLINE CSS IN JSX
+    >
+      <h3>Meghana Foods</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container"></div>
+      <Restaurantcard />
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading />);
+root.render(<AppLayout />);
