71a2bdd65ce754feefa79f19235d6cd1753bf08c
Venka <venkataganesh294@gmail.com>
2026-06-25 16:39:48 +0530

    cha

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,24 +5,35 @@ import resList from "../../utils/mockData.js";
 // not using keys (not acceptable) <<< index as key <<<< unique id (best practise)
 let Body = () => {
   // Local state variable - super powerful variable
-const [ListOfRestaurants, setListOfRestaurant] = useState([]);
+const [ListOfRestaurants, setListOfRestaurant] = useState(resList);
 
-  useEffect(() => {
-  setListOfRestaurant(resList);
+  useEffect(() => { 
+    fetchData();    // Use to fetch the data 
 }, []);
 // this callback function is called after the component renders (after body component renders as soon as body component render cycle is finished it will call the callback funtion that is passed into a useEffect after that it prints "useEffect is called" that is present inside the console.log )
 
   // console.log("body rendered");  // Body is rendered first and that useEffect is rendered
+   
 
 
-  const fetchData = ()  => {
-    const data = [fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.3843&lng=78.4583&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")]
-  }
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.461421&lng=78.3346205&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
+    const json = await data.json();
+
+    console.log(json);
+};
+
+  // Call fetchData()
+  //       ↓
+  // Make HTTP GET request to Swiggy API
+  //       ↓
+  // Wait (await) for response...
+  //       ↓
+  // Store result in `data`
   
-
-
-
   return (
     <div className="body">
       <div className="filter">
